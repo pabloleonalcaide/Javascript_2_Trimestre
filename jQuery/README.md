d5cbb2718b7ca106904d95dd7f0fe5487d1cbdd8
pabloleonalcaide <pabloleonpsico@gmail.com>
2018-01-18 14:30:35 +0100

    created jQuery exercises folder

---
+++ jQuery/README.md
@@ -0,0 +1,3 @@
+# Ejercicios con jQuery
+
+Ejercicios de jQuery del manual de desarrolloweb.com
---
+++ jQuery/demo2/demo2_PabloLeon.js
@@ -0,0 +1,6 @@
+	$('#capa').mouseenter(function(evento){
+		$("#mensaje").css('display', 'block');
+	});
+	$('#capa').mouseleave(function(evento){
+		$('#mensaje').css('display', 'none');
+	});
