578f1007f0f218c468e46dd80f0f0342fadc5d5b
pabloleonalcaide <pabloleonpsico@gmail.co>
2018-02-10 16:44:33 +0100

    insert function fail in $get

--- Ajax/ajax2/js/ejercicio1.js
+++ Ajax/ajax2/js/ejercicio1.js
@@ -1,9 +1,11 @@
 $(document).ready(function() {
         $("#enviar").click(function(){
-          let url = $('#recurso').html();
-          $.get('index.html',function(data,status,xhr){
+          let url = $('#recurso').val();
+          $.get(url,function(data,status,xhr){
             $('#estados').html(xhr.status + ' '+ xhr.statusText);
             $('#contenidos').text(xhr.responseText);
+          }).fail(function(){
+            $('#contenidos').html('Recurso no encontrado');
           })
     });
   });
