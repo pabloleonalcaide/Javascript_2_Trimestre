812cc6070bb164333125e4ac679828033eb4aa3d
pabloleonalcaide <pabloleonpsico@gmail.co>
2018-02-18 13:55:12 +0100

    more jquery less lines

--- mastermind usando jQuery/js/game.js
+++ mastermind usando jQuery/js/game.js
@@ -1,183 +1,179 @@
 /**
  * @author Pablo Leon Alcaide
- * @verson 1.2
+ * @verson 1.3
  */
 {
-    // counter defined as global to use it in different functions
-    var counter;
-    let linesCount;
-    let circlesToFill;
-    let circlesToCheck;
+    let $counter;
+    let $linesCount;
+    let $circlesToFill;
+    let $circlesToCheck;
 
+    /** 
+    * start a new game
+    * @see mastermind.js
+    */
+    let init = function() {
+        mastermind.init();
+        mastermind.mostrar();
+        $circlesToFill = $(".circlesToFill");
+        $circlesToCheck = $(".circlesToCheck");
+        $linesCount = 0;
+        $counter = 0;
+ 		$( "#dialog" ).dialog({ autoOpen: false });
+        $('h1').hide().show( "fold", 2000);
+        $('#check').click(function(){
+        	checkRow();
+            $(this).effect("shake");
+        })
+        $('#reset').click(init);
+        $('.circle').on("click", insertCircle); //insert a color event
+        $('.circle').click(function(){    
+        	$(this).effect("bounce");
+        })
+        generateNewLine();
+    }
+
+    let isEmptyCircle = (circle)=>{
+        if(circle.style.backgroundColor == "transparent" || circle.style.backgroundColor == "" ||circle.style.backgroundColor == "none")
+            return true
+        return false
+    }
     /**
      * Insert a color into the row if there is space
      */
     let insertCircle = function() {
-        for (let i = 0; i < circlesToFill.length; i++) {
-            //check where is the first empty space
-            if (circlesToFill[i].style.backgroundColor == "transparent" || circlesToFill[i].style.backgroundColor == "" ||
-                circlesToFill[i].style.backgroundColor == "none") {
-                switch (this.id) {
+        $clickedColor = this.id;
+        $circlesToFill.each(function(index,element){
+            if (isEmptyCircle(element)) {
+                switch ($clickedColor) {
                     case "redCircle":
-                        circlesToFill[i].style.backgroundColor = 'red'; break;
+                        element.style.backgroundColor = 'red'; break;
                     case "greenCircle":
-                        circlesToFill[i].style.backgroundColor = 'green'; break;
+                        element.style.backgroundColor = 'green'; break;
                     case "blueCircle":
-                        circlesToFill[i].style.backgroundColor = 'blue'; break;
+                        element.style.backgroundColor = 'blue'; break;
                     case "yellowCircle":
-                        circlesToFill[i].style.backgroundColor = 'yellow'; break;
+                        element.style.backgroundColor = 'yellow'; break;
                     case "brownCircle":
-                        circlesToFill[i].style.backgroundColor = 'brown'; break;
+                        element.style.backgroundColor = 'brown'; break;
                     case "orangeCircle":
-                        circlesToFill[i].style.backgroundColor = 'orange'; break;
+                        element.style.backgroundColor = 'orange'; break;
                     case "whiteCircle":
-                        circlesToFill[i].style.backgroundColor = 'white'; break;
+                        element.style.backgroundColor = 'white'; break;
                     case "blackCircle":
-                        circlesToFill[i].style.backgroundColor = 'black'; break;
+                        element.style.backgroundColor = 'black'; break;
                 }
-                $(circlesToFill[i]).on("click", clean);
-                break;
+                $(element).on("click", clean);
+                return false;
             }
-        }
-        if (counter < 4)
-            counter++;
+        })
+        if ($counter < 4)
+            $counter++;
     }
     /**
      * Create a new row 
      */
     let generateNewLine = () => {
-        removeCleanEvent();
-        let newRow = $('<div id="newRow"></div>');
-        $('#playPanel').append(newRow);
-        let rowCirclesToFill = $('<div id="circlesToFill"></div>');
-        let rowCirclesToCheck = $('<div id="circlesToCheck"></div>');
-        newRow.append(rowCirclesToFill);
-        newRow.append(rowCirclesToCheck);
+        $('.circleFill').off("click");
+        let $newRow = $('<div id="$newRow"></div>');
+        $('#playPanel').append($newRow);
+        let $rowCirclesToFill = $('<div id="circlesToFill"></div>');
+        let $rowCirclesToCheck = $('<div id="circlesToCheck"></div>');
+        $newRow.append($rowCirclesToFill);
+        $newRow.append($rowCirclesToCheck);
 
-        let emptyCircle;
-        let checkCircle;
+        let $emptyCircle;
+        let $checkCircle;
         /* Insert the row of circles to paint */
         for (let i = 0; i < 4; i++) {
-            emptyCircle = document.createElement("div");
-            emptyCircle.classList.add("circleFill", "circleFill" + linesCount);
-
-            rowCirclesToFill.append(emptyCircle);
+            $emptyCircle = $('<div class="circleFill circleFill'+$linesCount+'"></div>');
+            $rowCirclesToFill.append($emptyCircle);
         }
         /* Insert the row of check circles */
         for (let i = 0; i < 4; i++) {
-            checkCircle = document.createElement("div");
-            checkCircle.classList.add("circleCheck", "circleCheck" + linesCount);
-            rowCirclesToCheck.append(checkCircle);
+            $checkCircle = $('<div class="circleCheck circleCheck'+$linesCount+'"></div>');
+            $rowCirclesToCheck.append($checkCircle);
         }
 
-        counter = 0;
-        circlesToFill = document.getElementsByClassName("circleFill" + linesCount);
-        circlesToCheck = document.getElementsByClassName("circleCheck" + linesCount);
-        linesCount++;
+        $counter = 0;
+        $circlesToFill = $(".circleFill" + $linesCount);
+        $circlesToCheck = $(".circleCheck" + $linesCount);
+        $linesCount++;
+        $('#playPanel').animate({ scrollTop: 0 }, "slow");
     }
-
-    let check = () => {
-        let arrayToCheck = [];
-        let counter2 = 0;
-        for (let i = 0; i < circlesToFill.length; i++) {
-            switch (circlesToFill[i].style.backgroundColor) {
+    /**
+    * check the row
+    */
+    let checkRow = () => {
+        $arrayToCheck = [];
+        $hits = 0;
+        $circlesToFill.each(function(index,element){
+            switch (element.style.backgroundColor) {
                 case "red":
-                    arrayToCheck.push("rojo"); break;
+                    $arrayToCheck.push("rojo"); break;
                 case "white":
-                    arrayToCheck.push("blanco"); break;
+                    $arrayToCheck.push("blanco"); break;
                 case "black":
-                    arrayToCheck.push("negro"); break;
+                    $arrayToCheck.push("negro"); break;
                 case "green":
-                    arrayToCheck.push("verde"); break;
+                    $arrayToCheck.push("verde"); break;
                 case "blue":
-                    arrayToCheck.push("azul"); break;
+                    $arrayToCheck.push("azul"); break;
                 case "yellow":
-                    arrayToCheck.push("amarillo"); break;
+                    $arrayToCheck.push("amarillo"); break;
                 case "brown":
-                    arrayToCheck.push("marron"); break;
+                    $arrayToCheck.push("marron"); break;
                 case "orange":
-                    arrayToCheck.push("naranja"); break;
+                    $arrayToCheck.push("naranja"); break;
             }
-        }
+            
+        })
 
-        if (counter >= 4) {
-            rowChecked = mastermind.comprobarCoincidencia(arrayToCheck);
-            if (rowChecked.inSite > 0) {
-                while (counter2 < rowChecked.inSite) {
-                    circlesToCheck[counter2].style = "background-color: black;";
-                    counter2++;
+        if ($counter >= 4) {
+            $rowChecked = mastermind.comprobarCoincidencia($arrayToCheck);
+            if ($rowChecked.inSite > 0) {
+                while ($hits < $rowChecked.inSite) {
+                    $circlesToCheck[$hits].style = "background-color: black;";
+                    $hits++;
                 }
             }
 
-            if (counter2 == 4) {
- 				$( "#dialog" ).dialog("open");
-                    $( "#dialog" ).dialog({
-					      resizable: false,
-					      height: "auto",
-					      width: 400,
-					      modal: true,
-					      buttons: {
-					        "Partida Nueva": function() {
-					          $( this ).dialog( "close" );
-					          location.reload();
-					        },
-					        "Salir": function() {
-					          $( this ).dialog( "close" );
+            if ($hits == 4)
+                openWinnerDialog();
+            else
+                generateNewLine();
 
-					        }
-					      }
-				   });
-
-            }
-
-            if (rowChecked.displaced > 0) {
-                for (let i = 0; i < rowChecked.displaced; i++) {
-                    circlesToCheck[counter2].style = "background-color: white;";
-                    counter2++;
+            if ($rowChecked.displaced > 0) {
+                for (let i = 0; i < $rowChecked.displaced; i++) {
+                    $circlesToCheck[$hits].style = "background-color: white;";
+                    $hits++;
                 }
-                counter2 = 0;
+                $hits = 0;
             }
-            //generate a new line and redirect the window    
-            generateNewLine();
-            $('#playPanel').animate({ scrollTop: 0 }, "slow");
         }
     }
     /** clean a painted circle */
     let clean = function() {
-        counter--;
+        $counter--;
         $(this).css("background-color", "transparent");
-        this.removeEventListener("click", clean);
-
+        $(this).off("click", clean);
     }
-    /** restart the game*/
-    let reset = () => {
-        init();
+    let openWinnerDialog = ()=>{
+        $( "#dialog" ).dialog("open");
+                    $( "#dialog" ).dialog({
+                          resizable: false,
+                          height: "auto",
+                          width: 420,
+                          modal: true,
+                          buttons: {
+                            "Partida Nueva": function() { $(this).dialog( "close" );location.reload();},
+                            "Salir": function() {$( this ).dialog( "close" );}
+                          }
+                   }).position({
+                       my: "center",
+                       at: "center",
+                       of: window
+                    });
     }
-    /** remove the Clean event in the previous row */
-    let removeCleanEvent = function() {
-        $('.circleFill').off("click");
-
-    }
-    /** start a new game */
-    let init = function() {
-        mastermind.init();
-        mastermind.mostrar();
-        circlesToFill = $(".circlesToFill");
-        circlesToCheck = $(".circlesToCheck");
-        linesCount = 0;
-        counter = 0;
- 		$( "#dialog" ).dialog({ autoOpen: false });
-        $('h1').hide().show( "fold", 2000);
-        $('#check').click(check)
-        $('#check').click(function(){
-        	$(this).effect("shake");
-        })
-        $('#reset').click(reset);
-        $('.circle').on("click", insertCircle); //insert a color event
-        $('.circle').click(function(){
-        	$(this).effect("bounce");
-        })
-        generateNewLine();
-    }
-    $(document).ready(init);
+    $().ready(init);
 }
