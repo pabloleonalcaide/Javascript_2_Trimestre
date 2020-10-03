6ffe1bc52bbe0d7e56487a5ac75aae3c0b603622
NievesBorrero <nievesborrerobarea@gmail.com>
2020-10-03 13:28:11 +0200

    first improve

--- Proyecto-Dwamana/js/listado.js
+++ Proyecto-Dwamana/js/listado.js
@@ -1,72 +1,40 @@
 {
-	let callback = () =>{
+	const callback = () =>{}
+	const $list;
+	const lightboxOptions = {
+		'resizeDuration': 200,
+		'wrapAround': true,
+		'albumLabel': "Ponentes"
 	}
-	let $lista;
-	let $ponentes;
-	let init = ()=>{
-		$lista = $('#lista');
-		lightbox.option({
-	      'resizeDuration': 200,
-	      'wrapAround': true,
-	      'albumLabel': "Ponentes"
-	    })
-		cargarListadoPonentes(callback);
-	}
-	/**
-	* Carga toda la lista de ponentes
-	*/
-	let cargarListadoPonentes = ()=>{
-		mostrarDia('Lunes');		
-		mostrarDia('Martes');
-		mostrarDia('Miercoles');
-		mostrarDia('Jueves');
-		mostrarDia('Viernes');
-	}
-	
-	/**
-	* Muestra los ponentes de todos los días
-	*/
-	let mostrarPonentes = (datos)=>{
 
+	const init = ()=>{
+		$list = $('#lista');
+		lightbox.option(lightboxOptions)
+		loadSpeakersList(callback);
+	}
+
+	const loadSpeakersList = ()=>{
+		weekDays.each((day) => showDay(day))
+	}
+
+	const showSpeakers = (datos)=>{
 		$.each(datos,function(key,value){
-		let $div = $('<div class="ponente"></div>')
-		let $imagen = $('<img>').prop('src',value.imagen);
-		let $enlace =  $('<a data-lightbox="roadtrip"></a>').prop("href",value.imagen);
-		$enlace.append($imagen);
-		let $ponente = $('<p></p>').html(value.ponente);
-		$div.append($enlace,$ponente);
-		$lista.append($div);
+			let $div = $('<div class="ponente"></div>')
+			const $image = $('<img>').prop('src',value.imagen);
+			let $link =  $('<a data-lightbox="roadtrip"></a>').prop("href",value.imagen);
+			const $speaker = $('<p></p>').html(value.ponente);
+
+			$link.append($image);
+			$div.append($link, $speaker);
+			$list.append($div);
 		});
 	}
-	/**
-	* muestra los ponentes del día indicado
-	*/
-	let mostrarDia =(dia)=>{
-		switch(dia){
-			case 'Lunes':
-				$.getJSON('./js/lunes.json', function(data) {
-					mostrarPonentes(data['actividades']);	
-				}).done(function() {
-   					 $('.ponente').first().remove();  //Eliminamos la tarjeta duplicada de Nacho
-  				});break;
-			case 'Martes':
-				$.getJSON('./js/martes.json', function(data) {
-					mostrarPonentes(data['actividades']);	
-				});break;
-			case 'Miercoles':
-				$.getJSON('./js/miercoles.json', function(data) {
-					mostrarPonentes(data['actividades']);	
-				});break;
-			case 'Jueves':
-				$.getJSON('./js/jueves.json', function(data) {
-					mostrarPonentes(data['actividades']);	
-				});break;
-			case 'Viernes':
-				$.getJSON('./js/viernes.json', function(data) {
-					mostrarPonentes(data['actividades']);	
-				})
-				;break;
-		}
+
+	const showDay =(day)=>{
+		$.getJSON(`./js/${day}.json`, function(data) {
+			showSpeakers(data['actividades']);
+		})
 	}
+
 	$(init);
 }
